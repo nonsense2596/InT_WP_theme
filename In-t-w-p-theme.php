33e75cae3275643ded786d92fbb67579b74d1f7c
@nonsense2596 <peter.szatmary@windowslive.com>
2020-07-23 11:55:09 +0200

    test files add

---
+++ In-t-w-p-theme.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: In T WP Theme
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: Peter
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
