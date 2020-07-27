0ce2a14914e888f50cb607e67ee57268a37d9791
@nonsense2596 <peter.szatmary@windowslive.com>
2020-07-28 01:17:55 +0200

    finalized navbar animation and behaviour, final coloring left

--- scripts.js
+++ scripts.js
@@ -1,20 +1,25 @@
 /* navbar scroll */
 jQuery(document).ready(function() {
+
     jQuery(window).on("scroll",function(){
-        if(jQuery(document).scrollTop() >50){
-            jQuery('.navbar').addClass('testing');
-            /*jQuery('.navbar').animate({
-                    backgroundColor: "#ffffff"
-            }, 100
-            );*/
+        if(jQuery(document).scrollTop() >50 ){
+            jQuery('.navbar').addClass('navbar-white-scroll');
         }
         else{
-            jQuery('.navbar').removeClass('testing');
-            /*jQuery('.navbar').animate({
-                    backgroundColor: "#000000"
-                }, 1000
-            );*/
+            jQuery('.navbar').removeClass('navbar-white-scroll');
         }
     });
+
+
+    jQuery('.navbar-toggler').on("click",function(){
+        if(jQuery('.navbar').hasClass('navbar-white-scroll')){
+            jQuery('.navbar').removeClass('navbar-white-scroll');
+        }
+        else{
+            jQuery('.navbar').addClass('navbar-white-scroll');
+        }
+    });
+
 });
+
 
